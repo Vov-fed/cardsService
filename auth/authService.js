af4ecb3234662bca08582f09e886936a91a2eee9
Vovan-fedoruk <vova.fedoruk@icloud.com>
2025-01-20 16:43:08 +0200

    modified:   auth/authService.js

--- auth/authService.js
+++ auth/authService.js
@@ -32,38 +32,4 @@ const auth = (req, res, next) => {
   return handleError(res, 500, "The server not used valid token generator");
 };
 
-const admin = (req, res, next) => {
-  if (tokenGenerator === "jwt") {
-    try {
-      const tokenFromClient = req.header("x-auth-token");
-
-      if (!tokenFromClient) {
-        const error = new Error("Please Login");
-        error.status = 401;
-        return createError("Authentication", error);
-      }
-
-      const userInfo = verifyToken(tokenFromClient);
-      if (!userInfo) {
-        const error = new Error("Unauthorized user");
-        error.status = 401;
-        return createError("Authentication", error);
-      }
-
-      if (!userInfo.isAdmin) {
-        const error = new Error("Unauthorized user");
-        error.status = 401;
-        return createError("Authentication", error);
-      }
-
-      req.user = userInfo;
-      return next();
-    } catch (error) {
-      return handleError(res, 401, error.message);
-    }
-  }
-  return handleError(res, 500, "The server not used valid token generator");
-};
-
-
-module.exports = { auth, admin };
+module.exports = auth;
