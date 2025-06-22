b80f1064b14e767b24b463b8e7b03841fd710e0c
Vov-fed <138674234+Vov-fed@users.noreply.github.com>
2025-06-22 14:13:20 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -1,11 +1,18 @@
 const cors = require("cors");
 
-const corsMiddleware = cors({
+const corsOptions = {
   origin: [
-    "//https://f-card-lyart.vercel.app/",
-    "https://cardsservice.onrender.com",
-    "http://localhost:3000/"
+    'https://f-card-lyart.vercel.app',
+    'https://cardsservice.onrender.com',
+    'http://localhost:10000'
   ],
-});
+  credentials: true,
+  exposedHeaders: ['Content-Length', 'X-Total-Count'],
+  preflightContinue: false,
+  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS', 'PATCH'],
+  allowedHeaders: ['Content-Type', 'Authorization'],
+};
+
+const corsMiddleware = cors(corsOptions)
 
 module.exports = corsMiddleware;
