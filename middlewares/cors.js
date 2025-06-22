7ffa923f2696bf423bcb82a65ab3b96f1716107f
Vov-fed <138674234+Vov-fed@users.noreply.github.com>
2025-06-22 14:06:35 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -3,7 +3,7 @@ const cors = require("cors");
 const corsMiddleware = cors({
   origin: [
     "//https://f-card-lyart.vercel.app/",
-    "https://cardsservice.onrender.com"
+    "https://cardsservice.onrender.com",
     "http://localhost:3000/"
   ],
 });
