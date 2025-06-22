a066b693209adef0b7e401d120290796b65e5678
Vov-fed <138674234+Vov-fed@users.noreply.github.com>
2025-06-22 14:01:18 +0300

    Update cors.js

--- middlewares/cors.js
+++ middlewares/cors.js
@@ -2,16 +2,7 @@ const cors = require("cors");
 
 const corsMiddleware = cors({
   origin: [
-    "http://127.0.0.1:5500",
-    "www.cardsproject.co.il",
-    "http://localhost:5173",
-    "http://localhost:3000",
-    "https://react-project-tqe4.onrender.com",
-    "https://cardsservice.onrender.com",
-    "https://fedoruk-cards.vercel.app",
-    "https://fedoruk-cards-git-main-vov-feds-projects.vercel.app",
-    "https://fedoruk-cards-vov-feds-projects.vercel.app",
-    "https://astonishing-daffodil-cf4e9a.netlify.app"
+    *
   ],
 });
 
